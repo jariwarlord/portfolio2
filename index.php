a9670d6c23261e32e06b965733aa4ad5723c62b5
Berke Dalar <berkedalar4@gmail.com>
2023-07-02 20:24:26 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!---Css -->
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <title>Porfolio</title>
+</head>
+<body>
+<nav class = "navbar navbar-dark bg-dark justify-content-center fs-3 mb-5" style="color:whitesmoke">
+
+    My Portfolio
+</nav>
+
+<div class="container-lg text-center margin: auto;">
+<h2>Web Developer</h2>
+    <p>I design web sites and i enjoy what i am doing.. </p>
+</div>
+<div class="container-lg text-center">
+    <img src="photo.png">
+</div>
+
+<div class="container" >
+    <img src="photo.png" alt="image">
+    <div class="text">About Me
+        <p id="p2">Desc</p>
+    </div>
+    <div class="photos">
+
+        <img src="php.png" alt="php">
+    </div>
+</div>
+
+
+
+
+
+<div class="container-lg text-center">
+    <a href="#">Projects</a>
+</div>
+<div class="container-lg text-center">
+    <a href="https://github.com/jariwarlord">Github</a>
+</div>
+<footer class="bg-dark text-center text-white">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+            <!-- Twitter -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+            ><i class="fab fa-twitter"></i
+                ></a>
+
+            <!-- Linkedin -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+            ><i class="fab fa-linkedin-in"></i
+                ></a>
+
+            <!-- Github -->
+            <a class="btn btn-outline-light btn-floating m-1" href="/github.com/jariwarlord" role="button"
+            ><i class="fab fa-github"></i
+                ></a>
+        </section>
+        <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Copyright:Berke Dalar
+
+    </div>
+    <!-- Copyright -->
+</footer>
+</body>
+</html>
